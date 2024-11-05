e93a404eb333ec95a2518a25a38fb66e0f7ace03
Zoocookie <zoe27112002@gmail.com>
2024-11-05 20:12:26 +0200

    Edit Form design.

--- PRG282 Project/Form1.Designer.cs
+++ PRG282 Project/Form1.Designer.cs
@@ -28,11 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
+            this.btnAddStudent = new System.Windows.Forms.Button();
+            this.btnDeleteStudent = new System.Windows.Forms.Button();
+            this.btnUpdateStudent = new System.Windows.Forms.Button();
+            this.btnViewStudent = new System.Windows.Forms.Button();
+            this.btnSummary = new System.Windows.Forms.Button();
             this.DBTable = new System.Windows.Forms.DataGridView();
             this.txName = new System.Windows.Forms.TextBox();
             this.txAge = new System.Windows.Forms.TextBox();
@@ -43,123 +43,129 @@
             ((System.ComponentModel.ISupportInitialize)(this.DBTable)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // btnAddStudent
             // 
-            this.button1.Location = new System.Drawing.Point(28, 284);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(148, 37);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Add Student";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnAddStudent.Location = new System.Drawing.Point(37, 350);
+            this.btnAddStudent.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnAddStudent.Name = "btnAddStudent";
+            this.btnAddStudent.Size = new System.Drawing.Size(197, 46);
+            this.btnAddStudent.TabIndex = 0;
+            this.btnAddStudent.Text = "Add Student";
+            this.btnAddStudent.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // btnDeleteStudent
             // 
-            this.button2.Location = new System.Drawing.Point(180, 284);
-            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(146, 37);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Delete Student";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnDeleteStudent.Location = new System.Drawing.Point(240, 350);
+            this.btnDeleteStudent.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnDeleteStudent.Name = "btnDeleteStudent";
+            this.btnDeleteStudent.Size = new System.Drawing.Size(195, 46);
+            this.btnDeleteStudent.TabIndex = 1;
+            this.btnDeleteStudent.Text = "Delete Student";
+            this.btnDeleteStudent.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // btnUpdateStudent
             // 
-            this.button3.Location = new System.Drawing.Point(330, 284);
-            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(130, 37);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Update Student";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnUpdateStudent.Location = new System.Drawing.Point(440, 350);
+            this.btnUpdateStudent.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnUpdateStudent.Name = "btnUpdateStudent";
+            this.btnUpdateStudent.Size = new System.Drawing.Size(173, 46);
+            this.btnUpdateStudent.TabIndex = 2;
+            this.btnUpdateStudent.Text = "Update Student";
+            this.btnUpdateStudent.UseVisualStyleBackColor = true;
             // 
-            // button4
+            // btnViewStudent
             // 
-            this.button4.Location = new System.Drawing.Point(450, 42);
-            this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(107, 28);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "View All Students";
-            this.button4.UseVisualStyleBackColor = true;
+            this.btnViewStudent.Location = new System.Drawing.Point(600, 52);
+            this.btnViewStudent.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnViewStudent.Name = "btnViewStudent";
+            this.btnViewStudent.Size = new System.Drawing.Size(143, 34);
+            this.btnViewStudent.TabIndex = 3;
+            this.btnViewStudent.Text = "View All Students";
+            this.btnViewStudent.UseVisualStyleBackColor = true;
             // 
-            // button5
+            // btnSummary
             // 
-            this.button5.Location = new System.Drawing.Point(450, 75);
-            this.button5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(104, 29);
-            this.button5.TabIndex = 4;
-            this.button5.Text = "Summary Report";
-            this.button5.UseVisualStyleBackColor = true;
-            this.button5.Click += new System.EventHandler(this.button5_Click);
+            this.btnSummary.Location = new System.Drawing.Point(600, 92);
+            this.btnSummary.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnSummary.Name = "btnSummary";
+            this.btnSummary.Size = new System.Drawing.Size(139, 36);
+            this.btnSummary.TabIndex = 4;
+            this.btnSummary.Text = "Summary Report";
+            this.btnSummary.UseVisualStyleBackColor = true;
+            this.btnSummary.Click += new System.EventHandler(this.button5_Click);
             // 
             // DBTable
             // 
             this.DBTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.DBTable.Location = new System.Drawing.Point(40, 10);
-            this.DBTable.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.DBTable.Location = new System.Drawing.Point(53, 12);
+            this.DBTable.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.DBTable.Name = "DBTable";
             this.DBTable.RowHeadersWidth = 51;
             this.DBTable.RowTemplate.Height = 24;
-            this.DBTable.Size = new System.Drawing.Size(380, 232);
+            this.DBTable.Size = new System.Drawing.Size(507, 286);
             this.DBTable.TabIndex = 5;
             this.DBTable.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
             this.DBTable.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // txName
             // 
-            this.txName.Location = new System.Drawing.Point(450, 145);
+            this.txName.Location = new System.Drawing.Point(600, 178);
+            this.txName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txName.Name = "txName";
-            this.txName.Size = new System.Drawing.Size(100, 20);
+            this.txName.Size = new System.Drawing.Size(132, 22);
             this.txName.TabIndex = 6;
             // 
             // txAge
             // 
-            this.txAge.Location = new System.Drawing.Point(450, 187);
+            this.txAge.Location = new System.Drawing.Point(600, 230);
+            this.txAge.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txAge.Name = "txAge";
-            this.txAge.Size = new System.Drawing.Size(100, 20);
+            this.txAge.Size = new System.Drawing.Size(132, 22);
             this.txAge.TabIndex = 7;
             // 
             // txCourse
             // 
-            this.txCourse.Location = new System.Drawing.Point(450, 234);
+            this.txCourse.Location = new System.Drawing.Point(600, 288);
+            this.txCourse.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txCourse.Name = "txCourse";
-            this.txCourse.Size = new System.Drawing.Size(100, 20);
+            this.txCourse.Size = new System.Drawing.Size(132, 22);
             this.txCourse.TabIndex = 8;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(467, 126);
+            this.label1.Location = new System.Drawing.Point(623, 155);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(44, 16);
             this.label1.TabIndex = 9;
             this.label1.Text = "Name";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(470, 172);
+            this.label2.Location = new System.Drawing.Point(627, 212);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.Size = new System.Drawing.Size(32, 16);
             this.label2.TabIndex = 10;
             this.label2.Text = "Age";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(467, 218);
+            this.label3.Location = new System.Drawing.Point(623, 268);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.Size = new System.Drawing.Size(50, 16);
             this.label3.TabIndex = 11;
             this.label3.Text = "Course";
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(582, 350);
+            this.ClientSize = new System.Drawing.Size(776, 431);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -167,12 +173,12 @@
             this.Controls.Add(this.txAge);
             this.Controls.Add(this.txName);
             this.Controls.Add(this.DBTable);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Controls.Add(this.btnSummary);
+            this.Controls.Add(this.btnViewStudent);
+            this.Controls.Add(this.btnUpdateStudent);
+            this.Controls.Add(this.btnDeleteStudent);
+            this.Controls.Add(this.btnAddStudent);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "MainForm";
             this.Text = "StudentRecord";
             this.Load += new System.EventHandler(this.MainForm_Load);
@@ -184,11 +190,11 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button btnAddStudent;
+        private System.Windows.Forms.Button btnDeleteStudent;
+        private System.Windows.Forms.Button btnUpdateStudent;
+        private System.Windows.Forms.Button btnViewStudent;
+        private System.Windows.Forms.Button btnSummary;
         private System.Windows.Forms.DataGridView DBTable;
         private System.Windows.Forms.TextBox txName;
         private System.Windows.Forms.TextBox txAge;
